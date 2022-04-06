09f6fa86d607053ec1ecb9324e93763d8e7f77cf
Duy Nguyen <nguyenduy.hust@gmail.com>
2022-04-06 18:20:36 +0700

    Update guswap subgraph url

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://graphnode.guswap.tk/subgraphs/name/guswap',
+    uri: 'https://graphnode.guswap.tk/subgraphs/name/gulabs/guswap',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
